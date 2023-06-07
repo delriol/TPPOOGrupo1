305172193e7c9262e0c63c22f891169e48508cad
agustinalejo1 <agustinalejoechaniz@gmail.com>
2023-06-07 18:19:42 -0300

    Agregar registro e iniciar sesion
    se agregaron los metodos Cliente.registrar2() y Cliente.iniciarSesion2()
    
    los nuevos clientes se agregan al ArrayList<Cliente> baseclientes
    la variable Cliente sesion_actual indica el cliente cuya sesion ha sido abierta, siendo null si aun no lo ha hecho.

--- src/Hoteles/Cliente.java
+++ src/Hoteles/Cliente.java
@@ -1,4 +1,7 @@
 package Hoteles;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Cliente {
     private String nombre;
@@ -159,4 +162,61 @@ public class Cliente {
 	}
 
 	public void reservar() {}
+
+	public static Cliente registrar2(Scanner scanner){
+
+		String nombre, apellido, domicilio, DNI, fechaNacimiento, datosTarjetaCredito,
+		email, contrasena, _cuentaBancaria, cuentaMercadoPago;
+		Cliente nuevo_cliente;
+
+		System.out.println("Ingrese su Nombre:");
+		nombre = scanner.nextLine();
+		System.out.println("Ingrese su apellido:");
+		apellido = scanner.nextLine();
+		System.out.println("Ingrese su domicilio:");
+		domicilio = scanner.nextLine();
+		System.out.println("Ingrese su DNI:");
+		DNI = scanner.nextLine();
+		System.out.println("Ingrese su fecha de nacimiento:");
+		fechaNacimiento = scanner.nextLine();
+		System.out.println("Ingrese sus datos de Tarjeta de Credito:");
+		datosTarjetaCredito = scanner.nextLine();
+		System.out.println("Ingrese su email:");
+		email = scanner.nextLine();
+		System.out.println("Ingrese su contrasena:");
+		contrasena = scanner.nextLine();
+		System.out.println("Ingrese su cuentaBancaria:");
+		_cuentaBancaria = scanner.nextLine();
+		System.out.println("Ingrese su cuentaMercadoPago:");
+		cuentaMercadoPago = scanner.nextLine();
+
+		nuevo_cliente = new Cliente(nombre, apellido, domicilio,
+		DNI, fechaNacimiento, datosTarjetaCredito, email, contrasena);
+
+		System.out.print("\033c");
+		System.out.println("Usuario registrado exitosamente.\n");
+
+
+		return nuevo_cliente;
+	}
+	public static Cliente iniciarSesion2(Scanner scanner, ArrayList<Cliente> baseclientes){
+		Cliente cliente = null;
+		String _email, _contrasena;
+		System.out.println("Ingrese su email:");
+		_email = scanner.nextLine();
+		System.out.println("Ingrese su contraseña:");
+		_contrasena = scanner.nextLine();
+		for (Cliente i : baseclientes) {
+			if (i.getEmail().equals(_email) && i.getContrasena().equals(_contrasena)){
+				cliente = i;
+			}
+		}
+		System.out.print("\033c");
+		if (cliente != null){
+			System.out.println("Inicio de sesión exitoso.\n");
+		}else{
+			System.out.println("Email o contraseña incorrectos.\n");
+		}
+		return cliente;
+	}
 }
